bce1b14e02cf2ebee72bde734a77019705f43cd4
MVasili 34 <vasili.dubov@gmail.com>
2023-10-07 23:38:56 +0300

    Added Article Edition Page

--- MobileApp/NewsMobileApp/ViewsNative/AddingArticlePage.xaml.cs
+++ MobileApp/NewsMobileApp/ViewsNative/AddingArticlePage.xaml.cs
@@ -6,4 +6,21 @@ public partial class AddingArticlePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void PickImageButton_Clicked(object sender, EventArgs e)
+    {
+		FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+		if(photo is not null)
+		{
+			
+			FileImage.Source = ImageSource.FromFile(photo.FullPath);
+			FileImage.WidthRequest = 100;
+			FileImage.HeightRequest = 70;
+			FilePathLabel.Text = photo.FullPath;
+		}
+		else
+		{
+			await DisplayAlert("Ошибка", "Фото не добавлено", "ОК");
+		}
+    }
 }
