47e5f741e0e89e9b184cce9a787d61926d342eb6
MVasili 34 <vasili.dubov@gmail.com>
2023-10-28 23:31:13 +0300

    Created Docker Image

--- API/ArticleODataAPI/Program.cs
+++ API/ArticleODataAPI/Program.cs
@@ -40,13 +40,16 @@ builder.Services.AddHttpLogging(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
